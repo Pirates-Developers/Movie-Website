ebbb533e23c10bcf8073de26986ded680ebce3d1
NikaGogelia <gogelianika1@gmail.com>
2023-11-24 01:09:04 +0400

    add movie mock data

---
+++ src/data/MovieData.js
@@ -0,0 +1,50 @@
+export const MovieData = [
+  {
+    id: 1,
+    img: "/assets/images/movie-img-1.png",
+    name: "ფილმის სახელი",
+    rating: 9.5,
+  },
+  {
+    id: 2,
+    img: "/assets/images/movie-img-2.png",
+    name: "ფილმის სახელი",
+    rating: 9.5,
+  },
+  {
+    id: 3,
+    img: "/assets/images/movie-img-3.png",
+    name: "ფილმის სახელი",
+    rating: 9.5,
+  },
+  {
+    id: 4,
+    img: "/assets/images/movie-img-4.png",
+    name: "ფილმის სახელი",
+    rating: 9.5,
+  },
+  {
+    id: 5,
+    img: "/assets/images/movie-img-5.png",
+    name: "ფილმის სახელი",
+    rating: 9.5,
+  },
+  {
+    id: 6,
+    img: "/assets/images/movie-img-6.png",
+    name: "ფილმის სახელი",
+    rating: 9.5,
+  },
+  {
+    id: 7,
+    img: "/assets/images/movie-img-7.png",
+    name: "ფილმის სახელი",
+    rating: 9.5,
+  },
+  {
+    id: 8,
+    img: "/assets/images/movie-img-8.png",
+    name: "ფილმის სახელი",
+    rating: 9.5,
+  },
+];
