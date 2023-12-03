50b70603be85dec5c3275b0341aaf45861d893c5
george <giorgi.tayniashvili415@gmail.com>
2023-12-03 13:16:57 +0400

    main

--- src/components/all-movie-card/AllMovieCard.js
+++ src/components/all-movie-card/AllMovieCard.js
@@ -4,42 +4,48 @@ import { Link } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function AllMovieCard({ data }) {
+  const IMG_URL = "https://image.tmdb.org/t/p/w500";
+
   return (
     <div key={data.id} className="all-movie-card">
       <Link
       // to={`/movie/${data.name}`}
       >
-      {window.innerWidth > 900 &&
-        <img
-          src="/assets/icons/header-slide-icon/movie-card-play.svg"
-          alt="play-icon"
-          className="play-img"
-        />
-      }
+        {window.innerWidth > 900 && (
+          <img
+            src="/assets/icons/header-slide-icon/movie-card-play.svg"
+            alt="play-icon"
+            className="play-img"
+          />
+        )}
         <LazyLoadImage
-          className="all-movie-card-img"
-          src={data.photo}
-          alt="movie-img"
+          key={data.id}
+          src={`${IMG_URL}${data.poster_path}`}
+          alt={`${data.title} Poster`}
           effect="blur"
+          // className="all-movie-card-img"
+          // src={data.photo}
+          // alt="movie-img"
+          // effect="blur"
         />
         {window.innerWidth <= 900 ? (
           <div className="all-movie-title">
-            <h1>{data.name}</h1>
+            <h1>{data.title}</h1>
           </div>
         ) : (
           <>
             <div className="all-movie-card-body">
               <div>
                 <div className="all-movie-title">
-                  <h1>{data.name}</h1>
+                  <h1>{data.title}</h1>
                 </div>
                 <div className="all-movie-rating">
-                  <p>{data.imdb}</p>
+                  <p>{data.vote_average}</p>
                 </div>
               </div>
               <div>
                 <div className="all-movie-title-english">
-                  <h2>TItle</h2>
+                  <h2> genres </h2>
                 </div>
                 <div className="all-movie-imdb">
                   <p>IMDB</p>
@@ -47,10 +53,10 @@ function AllMovieCard({ data }) {
               </div>
               <div>
                 <div className="all-movie-genres">
-                  <p>{data.category}</p>
+                  <p>{data.original_language}</p>
                 </div>
                 <div className="all-movie-year">
-                  <h3>{data.year}</h3>
+                  <h3>{data.release_date}</h3>
                 </div>
               </div>
             </div>
--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,35 +1,73 @@
+import axios from "axios";
 import "./movies.css";
 import { useState, useEffect } from "react";
-import { movies } from "../../data/MovieData";
+// import { movies } from "../../data/MovieData";
 import SearchBox from "../../components/searchbox/SearchBox";
 import FilterBoxes from "../../components/FilterBoxes/FilterBoxes";
 import AllMovieCard from "../../components/all-movie-card/AllMovieCard";
+// import AllMovieCard from "../../components/all-movie-card/AllMovieCard";
 
 export default function Movies() {
-  // const [moviesToShow, setMoviesToShow] = useState([]);
-  // const [page, setPage] = useState(1);
+  const API_KEY = "api_key=b96be90fb5eb89b775dfd1f4c66779f1";
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const discoverURL = `${BASE_URL}/discover/movie?${API_KEY}`;
+  // const searchURL = `${BASE_URL}/search/movie?${API_KEY}`;
 
-  // const loadMovies = () => {
-  //   const startIndex = (page - 1) * 10;
-  //   const selectedMovies = movies.slice(startIndex, startIndex + 10);
-  //   setMoviesToShow(moviesToShow.concat(selectedMovies));
-  // };
+  const [movies, setMovies] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState("");
 
-  // useEffect(() => {
-  //   loadMovies();
-  // }, [page]);
+  useEffect(() => {
+    getMovies(discoverURL);
+  }, []);
 
-  // const handleScroll = (event) => {
-  //   const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
-  //   if (scrollHeight - scrollTop === clientHeight) {
-  //     setPage(page + 1);
+  const getMovies = async (url) => {
+    await axios
+      .get(url)
+      .then((res) => {
+        const getData = res.data.results;
+        setMovies(getData);
+        console.log(getData);
+      })
+      .catch((error) => {
+        console.error(`Error: ${error}`);
+      });
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (searchTerm) {
+  //     getMovies(`${searchURL}&query=${encodeURIComponent(searchTerm)}`);
   //   }
-  // };
+  // // };
+
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      const url = `${discoverURL}&page=${page}`;
+      try {
+        const response = await axios.get(url);
+        setMovies((prevMovies) => [...prevMovies, ...response.data.results]);
+      } catch (error) {
+        console.error(`Error: ${error}`);
+      }
+    };
+
+    loadMovies();
+  }, [page]);
+
+  const handleScroll = (event) => {
+    const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+    if (scrollHeight - scrollTop <= clientHeight * 1.5) {
+      // Adjust this threshold as needed
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
 
   return (
     <main
-    // onScroll={handleScroll}
-    // style={{ overflowY: "auto", height: "100vh" }}
+      onScroll={handleScroll}
+      style={{ overflowY: "auto", height: "100vh" }}
     >
       <div className="film-search-filter-div">
         <div className="film-div">
@@ -46,6 +84,9 @@ export default function Movies() {
         {movies.map((movie, index) => (
           <AllMovieCard data={movie} key={index} />
         ))}
+        {/* {movies.map((movie, index) => (
+          <AllMovieCard data={movie} key={index} />
+        ))} */}
       </div>
     </main>
   );
